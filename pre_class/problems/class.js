feea6ced16de32ebc5c607afcd2c7700394eab56
Mlkz <maho.mamulashvili@gmail.com>
2022-09-01 19:10:23 -0400

    slice array

---
+++ pre_class/problems/class.js
@@ -0,0 +1,13 @@
+// second argument in .slice() is inclusive.
+// No argument with print copy of the original array;
+
+// One argument will return same array but it will start from
+// an index that we passed as an argument rto a .slice();
+// .slice(); just returns new array.
+
+
+let array1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(array1.slice(2, 6));
+
+
+// .split()
